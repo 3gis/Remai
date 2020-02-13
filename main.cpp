0bf94acb51a7503c679e3c645486916ec55fac1f
3gis <52011465+3gis@users.noreply.github.com>
2020-02-13 15:25:13 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string eil1, eil2, eil3, eil4, eil5;
+    string vardas, pasisveikinimas;
+    cout << "Iveskite savo varda: ";
+    cin >> vardas;
+    pasisveikinimas = "Sveika, ";
+    cout << endl;
+    int dydis = pasisveikinimas.length() + vardas.length();
+    for(int i = 0; i<dydis+4;i++)
+        eil1+="*";
+    eil2 += "*";
+    for(int i = 0; i<dydis+2;i++)
+        eil2+=" ";
+    eil2+="*";
+    eil3+= "* " + pasisveikinimas + vardas + " *";
+    eil4=eil2;
+    eil5 = eil1;
+    cout << eil1 << endl << eil2 << endl << eil3 << endl << eil4 << endl << eil5;
+
+    return 0;
+}
